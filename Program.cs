2c462ba78cf3ba9dafc72c5d113769994cf39066
ZeyadGouda <zm41204@gmail.com>
2023-07-07 00:33:21 +0200

    refactor: replaced array with list

--- Program.cs
+++ Program.cs
@@ -1,52 +1,38 @@
-    Console.Write("Enter Maximum degree : ");
-    double MaximumDegree = Convert.ToDouble(Console.ReadLine());
-    Console.Write("Enter The Number Of Students : ");
-    int NumberOfStudents = Convert.ToInt32(Console.ReadLine()); ;
-    int num = 1;
-    object[,] School = new object[NumberOfStudents, 3];
+double MaximumDegree = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter The Number Of Students : ");
+int NumberOfStudents = Convert.ToInt32(Console.ReadLine()); ;
+int num = 1;
+School MySchool = new School();
+Student ObjectStudent;
+
 while (num <= NumberOfStudents)
 {
-    Student ObjectStudent = new Student();
+    ObjectStudent = new Student();
     try
     {
-        
         Console.Write($"Enter Name {num} : ");
         ObjectStudent.Name = Console.ReadLine();
         Console.Write($"Enter Birthdate {num}: ");
-        ObjectStudent.birthdate =Convert.ToDateTime(Console.ReadLine());
+        ObjectStudent.birthdate = Convert.ToDateTime(Console.ReadLine());
         Console.Write($"Enter degree {num} : ");
         ObjectStudent.degree = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Name : " + ObjectStudent.Name);
-        Console.WriteLine($"Birthdate : {ObjectStudent.birthdate}");
-       
-        Console.WriteLine(ObjectStudent.Age);
-        if (ObjectStudent.degree <= MaximumDegree && ObjectStudent.degree >= MaximumDegree / 2)
-        {
-            Console.WriteLine($"Degree : {ObjectStudent.degree}");
-            Console.WriteLine("Succeded");
-        }
-        else if (ObjectStudent.degree > MaximumDegree || ObjectStudent.degree < 0)
-        {
-            Console.WriteLine("Wrong input");
-        }
-        else
-        {
-            Console.WriteLine($"Degree : {ObjectStudent.degree}");
-            Console.WriteLine("Failed");
-        }
-
-
-        num++;
-
     }
     catch
     {
-        Console.WriteLine("Erorr");
+        Console.WriteLine("ERROR");
     }
-    for (int i = 0; i < NumberOfStudents; i++)
-    {
+        if
+            (ObjectStudent.degree > MaximumDegree || ObjectStudent.degree < 0)
+        {
+            Console.WriteLine("Wrong input");
+        }
+        
+        MySchool.Students.Add(ObjectStudent);
+
+        num++;
+ 
+
         Console.WriteLine($"Name: {ObjectStudent.Name}, Birthdate: {ObjectStudent.birthdate}, Degree: {ObjectStudent.degree}, Age: {ObjectStudent.Age}");
-    }
     string searchName;
     while (true)
     {
@@ -58,12 +44,12 @@ while (num <= NumberOfStudents)
 
         bool foundStudent = false;
 
-        for (int i = 0; i < NumberOfStudents; i++)
+        foreach (Student student in MySchool.Students)
         {
-            if (((string)School[i, 0]).Contains(searchName))
-            {
+            if (student.Name == searchName)
+                {
                 foundStudent = true;
-                Console.WriteLine($"Name: {ObjectStudent.Name}, Birthdate: {ObjectStudent.birthdate}, Degree: {ObjectStudent.degree}");
+                Console.WriteLine($"Name: {ObjectStudent.Name}, Birthdate: {ObjectStudent.birthdate}, Degree: {ObjectStudent.degree}, Age: {ObjectStudent.Age}");
             }
         }
         if (!foundStudent)
@@ -71,4 +57,8 @@ while (num <= NumberOfStudents)
             Console.WriteLine("No students found.");
         }
     }
+}
+foreach (Student student in MySchool.Students)
+{
+    Console.WriteLine($"Name: {student.Name}, Birthdate: {student.birthdate}, Degree: {student.degree}, Age: {student.Age}");
 }
